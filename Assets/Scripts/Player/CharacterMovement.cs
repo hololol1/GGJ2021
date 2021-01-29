0d614cf1dd1e48c2dc250df6753bd81db0a777ef
AnGeLoKr <angelo_kr@hotmail.it>
2021-01-29 21:46:22 +0000

    2D Platformer movement

---
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public LayerMask groundLayer;
+    public Transform groundPosition;
+
+    public float moveSpeed = 5.0f;
+    public float jumpForce = 200.0f;
+    private bool grounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics.OverlapSphere(groundPosition.position, 0.25f, groundLayer).Length > 0;
+        Debug.DrawLine(transform.position, transform.position + new Vector3(0.0f, -0.25f, 0.0f), Color.red);
+        Vector3 direction = Vector3.zero;
+        float d = Input.GetAxisRaw("Horizontal");
+        
+        if (d < 0.0f)
+        {
+            direction = Vector3.left;
+        }
+
+        if (d > 0.0f)
+        {
+            direction = Vector3.right;
+        }
+
+        transform.position = transform.position + (direction * moveSpeed * Time.deltaTime);
+        
+        if(Input.GetButtonDown("Jump") && grounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+        }
+
+    }
+}
