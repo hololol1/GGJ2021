e3ea8eba3dbd1f945ba26ccf7880d8ba529f013f
AnGeLoKr <angelo_kr@hotmail.it>
2021-01-30 21:39:25 +0000

    Level change camera script and game manager with ui manager

---
+++ Assets/Scripts/Level/LevelTrigger.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class LevelTrigger : MonoBehaviour
+{
+    private Collider coll;
+    public int levelNumber = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coll = GetComponent<Collider>();
+        coll.isTrigger = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            GameManager.Instance.ChangeLevel(levelNumber);
+        }
+    }
+}
