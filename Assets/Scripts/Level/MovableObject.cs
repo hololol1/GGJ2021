8ac9362543d5e2abce7fd2f0ee372a3420bce832
Marvin <44087233+marvinspahn@users.noreply.github.com>
2021-01-31 13:56:47 +0100

    added sound implemetation to box/movable object

--- Assets/Scripts/Level/MovableObject.cs
+++ Assets/Scripts/Level/MovableObject.cs
@@ -6,9 +6,32 @@ using UnityEngine;
 public class MovableObject : InteractableObject
 {
     private Rigidbody rb;
+    private AudioSource[] audioPlayer;
+    public AudioClip[] objectHitSounds;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioPlayer = GetComponents<AudioSource>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        audioPlayer[0].clip = objectHitSounds[Random.Range(0,objectHitSounds.Length-1)];
+        audioPlayer[0].Play();
+    }
+    private void Update()
+    {
+        if (rb.velocity.x > 0.01f || rb.velocity.x < -0.01f)
+        {
+            if (!audioPlayer[1].isPlaying)
+            {
+                audioPlayer[1].Play();
+                audioPlayer[1].time = Random.Range(0, audioPlayer[1].clip.length);
+            }
+        }
+        else
+        {
+            audioPlayer[1].Stop();
+        }
     }
 }
