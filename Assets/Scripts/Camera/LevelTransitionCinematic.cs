d94b984923908aec3d44b128f8073856a804d86a
JanosWey <60521716+JanosWey@users.noreply.github.com>
2021-02-01 00:25:19 +0100

    changed intro video setup to work now and added tape

--- Assets/Scripts/Camera/LevelTransitionCinematic.cs
+++ Assets/Scripts/Camera/LevelTransitionCinematic.cs
@@ -8,29 +8,55 @@ public class LevelTransitionCinematic : MonoBehaviour
     public double time;
     public double currentTime;
 
+    [SerializeField]
+    private string _movieFilename;
+
     // Start is called before the first frame update
     void Start()
     {
         videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer>();
-       
 
-        string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, "Intro.mp4");
-        videoPlayer.url = filePath;
+        StartCoroutine(PlayMovie(_movieFilename));
 
-        //videoPlayer.renderMode = VideoRenderMode.CameraNearPlane;
-        //videoPlayer.targetCameraAlpha = 1.0f;
-       videoPlayer.Play();
-
-        time = videoPlayer.length;
+        //time = videoPlayer.length;
     }
 
     // Update is called once per frame
     void Update()
     {
-       /* currentTime = videoPlayer.time;
-        //if(currentTime >= time-0.1f)
+        currentTime = videoPlayer.time;
+        if(currentTime >= time-0.1f)
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
-        }*/
+        }
     }
+
+    /// <summary>
+    /// Stream the specified video.
+    /// </summary>
+    /// <param name="filename">The video file.</param>
+    /// <returns>Coroutine.</returns>
+    private IEnumerator PlayMovie(string filename)
+    {
+        //VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
+        if (videoPlayer)
+        {
+            // It's important that the video is in /Assets/StreamingAssets
+            string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, _movieFilename);
+
+            //Debug.Log($"About play video: {_movieFilename}");
+
+            videoPlayer.url = videoPath;
+
+            videoPlayer.Play();
+            while (videoPlayer.isPlaying)
+            {
+                yield return null;
+            }
+
+            videoPlayer.Stop();
+        }
+    }
+
+
 }
